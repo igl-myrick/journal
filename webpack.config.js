a6a15d29da3fbe02b12bbe65722c4cf47f55b224
igl-myrick <igl.myrick@gmail.com>
2024-04-05 15:42:55 -0700

    add eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -10,6 +11,7 @@ module.exports = {
   },
   devtool: 'eval-source-map',
   plugins: [
+    new ESLintPlugin(),
     new CleanWebpackPlugin({
       verbose: true
     }),
