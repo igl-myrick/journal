2bdf4c544f62d8c04d3348cd60a289de4a3b0719
igl-myrick <igl.myrick@gmail.com>
2024-04-08 15:32:39 -0700

    add countVowels prototype

--- src/journal.js
+++ src/journal.js
@@ -2,3 +2,14 @@ function Entry(titleText, bodyText) {
   this.title = titleText;
   this.body = bodyText;
 }
+
+Entry.prototype.countVowels = function() {
+  const bodyArr = this.body.split("");
+  let result = 0;
+  bodyArr.forEach(function(element) {
+    if (element.match(/[aeiou]/gi)) {
+      result += 1;
+    }
+  });
+  return result;
+}
